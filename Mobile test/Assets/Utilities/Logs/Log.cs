58e79da33b22c0bb55cb3f233d34e7425cf180a6
ETU\d.harle <damien.harle@gmail.com>
2023-01-24 11:06:57 +0100

    yes

---
+++ Mobile test/Assets/Utilities/Logs/Log.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : MonoBehaviour
+{
+    public static void LogString(string message)
+    {
+        Debug.Log(message);
+    }
+}
